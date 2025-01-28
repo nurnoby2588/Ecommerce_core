811268a9df0ef1379d48cb27059f105a3bf2c33a
nurnoby admin <nurnoby323@gmail.com>
2025-01-28 21:38:12 +0600

    list product api added

---
+++ Models/Product.cs
@@ -0,0 +1,18 @@
+﻿using System.Runtime.Serialization;
+
+namespace Ecommerce_core.Models
+{
+    //[Serialization]
+    public class Product
+    {
+        [DataMember]
+        public string Name { get; set; }
+        [DataMember]
+        public string Picture { get; set; }
+        [DataMember]
+        public double  Price { get; set; }
+        [DataMember]
+        public int Quantity { get; set; }
+       
+    }
+}
