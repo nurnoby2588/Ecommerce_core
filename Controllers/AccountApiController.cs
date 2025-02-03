e4f112ed2da6a0461a7913d9276daf27af78595d
nurnoby admin <nurnoby323@gmail.com>
2025-02-03 10:33:24 +0600

    create account contoler , api and login view

---
+++ Controllers/AccountApiController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Ecommerce_core.Controllers
+{
+    public class AccountApiController : Controller
+    {
+        public IActionResult verifyUser(string userName, string password)
+        {
+            if (userName == "nurnoby" && password == "123456")
+            {
+                return Ok("success");
+            }
+            else
+            {
+                return Unauthorized(new {message = "Unauthorize access"});
+            }
+           
+        }
+    }
+}
---
+++ Controllers/AccountController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Ecommerce_core.Controllers
+{
+    public class AccountController : Controller
+    {
+        public IActionResult Login()
+        {
+            return View();
+        }
+    }
+}
